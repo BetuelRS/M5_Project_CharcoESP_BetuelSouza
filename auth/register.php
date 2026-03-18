ec6e3e0915d1ea0c8f70e01c497f0632239824a5
BetuelRS <betuel801@gmail.com>
2026-03-18 16:16:01 +0000

    Name

--- auth/register.php
+++ auth/register.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Criar Conta - EcoMonitor</title>
+    <title>Criar Conta - DashBoard ESP</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600&display=swap" rel="stylesheet">
